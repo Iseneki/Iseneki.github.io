1ef7b974d655d0968b2e775d1ced43ae49601e7f
Iseneki <55770788+Iseneki@users.noreply.github.com>
2021-10-17 20:06:44 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,115 @@
+window.addEventListener('load', init);
+
+const levels = {
+  easy: 7,
+  medium: 5,
+  hard: 3
+};
+
+const currentLevel = levels.easy;
+
+let time = currentLevel;
+let score = 0;
+let isPlaying;
+
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+const highscoreDisplay = document.querySelector('#highscore');
+
+const words = [
+  'flibbertigibbet',
+  'gobbledygook',
+  'gobemouche',
+  'lickety-split',
+  'skedaddle',
+  'hootenanny',
+  'widdershins',
+  'blitzed',
+  'glitterati',
+  'bumfuzzled',
+  'gardyloo',
+  'taradiddle',
+  'brewski',
+  'heebie-jeebies',
+  'collywobbles',
+  'bumbershoot',
+  'lollygag',
+  'snollygoster',
+  'brouhaha',
+  'sesquipedalian',
+  'argle-bargle',
+  'billingsgate',
+  'kerfuffle',
+  'namby-pamby',
+  'crepehanger'
+];
+
+function init() {
+  seconds.innerHTML = currentLevel;
+  showWord(words);
+  wordInput.addEventListener('input', startMatch);
+  setInterval(countdown, 1000);
+  setInterval(checkStatus, 50);
+}
+
+function startMatch() {
+  if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showWord(words);
+    wordInput.value = '';
+    score++;
+  }
+  
+  if (typeof sessionStorage['highscore'] === 'undefined' || score > sessionStorage['highscore']) {
+    sessionStorage['highscore'] = score;
+  } else {
+    sessionStorage['highscore'] = sessionStorage['highscore'];
+  }
+
+  if (sessionStorage['highscore'] >= 0) {
+  highscoreDisplay.innerHTML = sessionStorage['highscore'];
+  }
+
+  if (score === -1) {
+    scoreDisplay.innerHTML = 0;
+  } else {
+    scoreDisplay.innerHTML = score;
+  }
+}
+
+function matchWords() {
+  if (wordInput.value === currentWord.innerHTML) {
+    message.innerHTML = 'Correct!';
+    return true;
+  } else {
+    message.innerHTML = '';
+    return false;
+  }
+}
+
+function showWord(words) {
+  const randIndex = Math.floor(Math.random() * words.length);
+  currentWord.innerHTML = words[randIndex];
+}
+
+
+function countdown() {
+  if (time > 0) {
+    time--;
+  } else if (time === 0) {
+    isPlaying = false;
+  }
+  timeDisplay.innerHTML = time;
+}
+
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = 'Game Over!';
+    score = -1;
+  }
+}
